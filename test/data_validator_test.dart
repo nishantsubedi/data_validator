b0bab371aed3916506a129bf6665bfdb21828c4a
Helio S. Junior <helio.junior@codeminer42.com>
2019-02-06 10:44:35 -0200

    Adds CPF validates and tests

--- test/data_validator_test.dart
+++ test/data_validator_test.dart
@@ -6,4 +6,21 @@ void main() {
   test('check if is email', () {
     expect(DataValidator.isEmail("john@abc.com"), true);
   });
+
+  group('isValidCpf', () {
+    group('when is invalid', () {
+      test('it returns invalid', () {
+        expect(DataValidator.isValidCpf("00000000000"), false);
+        expect(DataValidator.isValidCpf("11111111111"), false);
+        expect(DataValidator.isValidCpf("anything"), false);
+      });
+    });
+
+    group('when is valid', () {
+      test('it returns valid', () {
+        expect(DataValidator.isValidCpf("55403106081"), true);
+        expect(DataValidator.isValidCpf("65925699050"), true);
+      });
+    });
+  });
 }
